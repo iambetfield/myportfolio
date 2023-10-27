c87631dad04117583502cd546da5f4538f57a9cc
betfield <nmalvarez@gmail.com>
2023-10-27 09:02:29 -0300

    updated info

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import CV from '../../assets/cv2023.pdf'
+import CV from '../../assets/CV/cv2023norbertoalvarezen.pdf'
 
 const CTA = () => {
   return (
     <div className='CTA'>
-        <a href={CV} download className='btn'> Download CV</a>
+       {/*  si quiero que el archivo se guarde solamente agregar download en la etiqueta */}
+        <a href={CV} className='btn' target='_blank'> Download CV</a> 
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
